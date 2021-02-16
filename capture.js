5e09208bd6de0380a97f11917639783030a7cbc4
c-not <68096835+c-not@users.noreply.github.com>
2021-02-17 00:45:36 +0530

    Update capture.js

--- capture.js
+++ capture.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   // The width and height of the captured photo. We will set the
   // width to the value defined here, but the height will be
   // calculated based on the aspect ratio of the input stream.
@@ -18,33 +18,40 @@
   var canvas = null;
   var photo = null;
   var startbutton = null;
+  var stopbutton = null;
+
+  // Custom variables
+  var intervalID = null;
+  var imageURLArray = [];
 
   function startup() {
     video = document.getElementById('video');
     canvas = document.getElementById('canvas');
     photo = document.getElementById('photo');
     startbutton = document.getElementById('startbutton');
+    stopbutton = document.getElementById('stopbutton');
+    sendbutton = document.getElementById('sendbutton');
 
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then(function(stream) {
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch(function(err) {
-      console.log("An error occurred: " + err);
-    });
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      .then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(function (err) {
+        console.log("An error occurred: " + err);
+      });
 
-    video.addEventListener('canplay', function(ev){
+    video.addEventListener('canplay', function (ev) {
       if (!streaming) {
-        height = video.videoHeight / (video.videoWidth/width);
-      
+        height = video.videoHeight / (video.videoWidth / width);
+
         // Firefox currently has a bug where the height can't be read from
         // the video, so we will make assumptions if this happens.
-      
+
         if (isNaN(height)) {
-          height = width / (4/3);
+          height = width / (4 / 3);
         }
-      
+
         video.setAttribute('width', width);
         video.setAttribute('height', height);
         canvas.setAttribute('width', width);
@@ -53,11 +60,18 @@
       }
     }, false);
 
-    startbutton.addEventListener('click', function(ev){
-      takepicture();
-      ev.preventDefault();
+    startbutton.addEventListener('click', function (ev) {
+      starttakingpictures(ev);
     }, false);
-    
+
+    stopbutton.addEventListener('click', function (ev) {
+      stoptakingpictures();
+    }, false);
+
+    sendbutton.addEventListener('click', function (ev) {
+      sendpictures();
+    }, false);
+
     clearphoto();
   }
 
@@ -72,25 +86,43 @@
     var data = canvas.toDataURL('image/png');
     photo.setAttribute('src', data);
   }
-  
+
   // Capture a photo by fetching the current contents of the video
   // and drawing it into a canvas, then converting that to a PNG
   // format data URL. By drawing it on an offscreen canvas and then
   // drawing that to the screen, we can change its size and/or apply
   // other changes before drawing it.
 
-  function takepicture() {
+  function takepicture(ev) {
     var context = canvas.getContext('2d');
     if (width && height) {
       canvas.width = width;
       canvas.height = height;
       context.drawImage(video, 0, 0, width, height);
-    
+
       var data = canvas.toDataURL('image/png');
+      imageURLArray.push(data);
       photo.setAttribute('src', data);
     } else {
       clearphoto();
     }
+    //ev.preventDefault();
+  }
+
+  // Capture images at an interval
+  function starttakingpictures(ev) {
+    intervalID = window.setInterval(takepicture, 1000, ev);
+  }
+
+  // Stop capturing images
+  function stoptakingpictures() {
+    window.clearInterval(intervalID);
+  }
+
+  // Send pictures
+  function sendpictures() {
+    console.log(imageURLArray);
+    alert(imageURLArray.length);
   }
 
   // Set up our event listener to run the startup process
